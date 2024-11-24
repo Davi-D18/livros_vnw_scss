f6105240820a2a46d9fa6233ec147f46d2f3d180
Davi-D18 <luiz.araujo@alunomicrolins.com.br>
2024-11-23 23:16:55 -0300

    arquivo de configuração do vite atualizado

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "192.168.1.15", // Permite que outros dispositivos acessem
+    host: "192.168.5.30", // Permite que outros dispositivos acessem
     port: 5173,
   },
 });

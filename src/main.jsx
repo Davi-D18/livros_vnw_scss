fbcedb75b6ee3d3c0ec359d3d39c5de88ea88216
Davi-D18 <luiz.araujo@alunomicrolins.com.br>
2024-12-13 11:43:58 -0300

    Atualizações na lib de rotas (react-router-dom)

--- src/main.jsx
+++ src/main.jsx
@@ -1,47 +1,57 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Home } from "./pages/Home/Home.jsx";
-import { FormDoacao } from "./pages/FormDoacao/FormDoacao.jsx";
-import { LivrosDoados } from "./pages/LivrosDoados/LivrosDoados.jsx";
-import { Resumo } from "./pages/Resumo/Resumo.jsx";
-import { Error404 } from "./pages/Error/Error404.jsx";
 import { App } from "./App.jsx";
 import { FormProvider } from "./context/FormProvider.jsx";
+import { Error404 } from "./pages/Error/Error404.jsx";
+import { FormDoacao } from "./pages/FormDoacao/FormDoacao.jsx";
+import { Home } from "./pages/Home/Home.jsx";
+import { LivrosDoados } from "./pages/LivrosDoados/LivrosDoados.jsx";
+import { Resumo } from "./pages/Resumo/Resumo.jsx";
 
 // Rotas das páginas do site
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      errorElement: <Error404 />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/doacao",
+          element: <FormDoacao />,
+        },
+        {
+          path: "/livrosDoados",
+          element: <LivrosDoados />,
+        },
+        {
+          path: "/resumo",
+          element: <Resumo />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-    errorElement: <Error404 />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/doacao",
-        element: <FormDoacao />,
-      },
-      {
-        path: "/livrosDoados",
-        element: <LivrosDoados />,
-      },
-      {
-        path: "/resumo",
-        element: <Resumo />,
-      },
-    ],
-  },
-]);
+    future: {
+      v7_startTransition: true,
+      v7_relativeSplatPath: true,
+      v7_fetcherPersist: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
+      v7_skipActionErrorRevalidation: true,
+    },
+  }
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <>
-      <FormProvider>
-        <RouterProvider router={router} />
-      </FormProvider>
-    </>
+    <FormProvider>
+      <RouterProvider router={router} />
+    </FormProvider>
   </StrictMode>
 );
